a43b9327d8b33f7cf5be7e4126143c290155510b
xinzhiFan <xfanac@umich.edu>
2017-04-17 18:30:32 -0400

    change str to float

--- data_process.py
+++ data_process.py
@@ -1,17 +1,35 @@
 import csv
 data_no_cyc = [];
+data_no_cyc_str = [];
+
 data_with_cyc = [];
+data_with_cyc_str = [];
+
 with open('test1.csv', 'rb') as csvfile:
-	data_pre = csv.reader(csvfile)
+	data_pre = csv.reader(csvfile, delimiter=',', quotechar='"')
 	for row in data_pre:
 		if row[4] != 'NULL':
-			data_with_cyc.append(row)
+			data_with_cyc_str.append(row)
 		elif row[4] == 'NULL':
 			del row[4:12]
-			data_no_cyc.append(row)
+			data_no_cyc_str.append(row)
 
-data_with_cyc_after = [];
-data_no_cyc_after = [];
+
+
+for row in data_with_cyc_str:
+	float_row = []
+	for col in row:
+		float_row.append( float(col))
+	data_with_cyc.append(float_row)
+
+for row in data_no_cyc_str:
+	float_row = []
+	for col in row:
+		float_row.append( float(col))
+	data_no_cyc.append(float_row)
+
+data_with_cyc_after = []
+data_no_cyc_after = []
 
 for i in range(len(data_with_cyc) - 9):
 	if  data_with_cyc[i][0] == data_with_cyc[i + 9][0]:
@@ -22,6 +40,7 @@ for i in range(len(data_no_cyc) - 9):
 	if  data_no_cyc[i][0] == data_no_cyc[i + 9][0]:
 		for j in range(10):
 			data_no_cyc_after.append(data_no_cyc[i + j])
+#print data_no_cyc_after
 
 with open("output1.csv", "wb") as f1:
     writer = csv.writer(f1)
