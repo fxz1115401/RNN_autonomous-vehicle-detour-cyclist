4245f9f335655f6fc101bd8afca95179ca7f3fdf
xinzhiFan <xfanac@umich.edu>
2017-04-17 23:15:12 -0400

    finished data processing

--- data_process.py
+++ data_process.py
@@ -28,26 +28,40 @@ for row in data_no_cyc_str:
 		float_row.append( float(col))
 	data_no_cyc.append(float_row)
 
-data_with_cyc_after = []
-data_no_cyc_after = []
+data_with_cyc_after_temp = []
+data_no_cyc_after_temp = []
 
 for i in range(len(data_with_cyc) - 9):
 	if  data_with_cyc[i][0] == data_with_cyc[i + 9][0]:
 		for j in range(10):
-			data_with_cyc_after.append(data_with_cyc[i + j])
+			data_with_cyc_after_temp.append(data_with_cyc[i + j])
 
 for i in range(len(data_no_cyc) - 9):
 	if  data_no_cyc[i][0] == data_no_cyc[i + 9][0]:
 		for j in range(10):
-			data_no_cyc_after.append(data_no_cyc[i + j])
+			data_no_cyc_after_temp.append(data_no_cyc[i + j])
 #print data_no_cyc_after
+
+#
+data_with_cyc_after = [];
+for i in range(len(data_with_cyc_after_temp))[0::10]:
+	data_with_cyc_after.append(data_with_cyc_after_temp[i:i+10])
+
+data_no_cyc_after = [];
+for i in range(len(data_no_cyc_after_temp))[0::10]:
+	data_no_cyc_after.append(data_no_cyc_after_temp[i:i+10])
+
+step = 1
+
+data_with_cyc_after[(step - 1) * 5:step * 5]
+
 
 with open("output1.csv", "wb") as f1:
     writer = csv.writer(f1)
-    writer.writerows(data_with_cyc_after)
+    writer.writerows(data_with_cyc_after_temp)
 
 with open("output2.csv", "wb") as f2:
     writer = csv.writer(f2)
-    writer.writerows(data_no_cyc_after)
+    writer.writerows(data_no_cyc_after_temp)
 
 
